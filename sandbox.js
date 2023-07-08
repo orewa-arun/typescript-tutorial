2b37feb9f6092c96156beed6b583718abfc38bd0
orewa-arun <arunworkacc0@gmail.com>
2023-07-08 14:47:37 +0530

    any

---
+++ sandbox.js
@@ -0,0 +1,12 @@
+// dynamic type
+var age;
+age = 30;
+age = "thirty";
+age = {
+    name: "ometic",
+    age: "30"
+};
+var mixed = [];
+mixed.push(3);
+mixed.push("mfg");
+var ninja;
---
+++ sandbox.ts
@@ -0,0 +1,19 @@
+// dynamic type
+let age : any;
+
+age = 30;
+age = "thirty";
+age = {
+    name : "ometic",
+    age : "30"
+}
+
+let mixed : any[] = [];
+
+mixed.push(3);
+mixed.push("mfg");
+
+let ninja : {
+    name : any,
+    age : any
+}
