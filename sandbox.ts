ef959e158a270ef472875d4e7499be2c0e4f9550
orewa-arun <arunworkacc0@gmail.com>
2023-07-08 11:42:33 +0530

    arrays and objects

--- sandbox.ts
+++ sandbox.ts
@@ -1,15 +1,21 @@
 // array
-var houses = ["adler", "orca"];
+
+let houses = ["adler","orca"];
+
 houses.push("lang");
 // shows error if you try to enter type that is not in the array
 // houses.push(3);
+
 //objects
-var student = {
-    name: "mash",
-    age: 17,
-    magic: "muscle"
-};
+let student = {
+    name : "mash",
+    age : 17,
+    magic : "muscle" 
+}
+
 // cannot change type of object parameters
 // student.age = "hello";
+
 student.age = 18;
-console.log(houses, student);
+
+console.log(houses,student)
