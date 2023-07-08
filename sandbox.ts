d21f70d4c038e3bd6bdf646b30c33f3a23a5ab26
orewa-arun <arunworkacc0@gmail.com>
2023-07-08 11:01:53 +0530

    ch1-\types

--- sandbox.ts
+++ sandbox.ts
@@ -1,12 +1,16 @@
 // In ts,the error checks are done before compiling it to js so
 // the compiled js may just be simple without the checks we did in ts
+
 // typescript infers the type
 // based on the value assigned
-var student = "mash";
-var age = 17;
-var HasMagic = true;
+let student = "mash";
+let age = 17;
+let HasMagic = true;
+
 // student = 20; // shows error
 HasMagic = false; // allowed
+
 // in ts ,we can define what type of argument one should receive
-var circumference = function (radius) { return 2 * radius * Math.PI; };
+const circumference = (radius : number) => 2*radius*Math.PI;
+
 console.log(circumference(7));
