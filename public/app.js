cc93da65a846119b80b97e4cda2c66ef52a1c30e
orewa-arun <arunworkacc0@gmail.com>
2023-07-09 09:05:06 +0530

    class defintion in ts

--- public/app.js
+++ public/app.js
@@ -1,4 +1,22 @@
 "use strict";
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        console.log(`${this.client} has to pay ${this.amount} for ${this.amount}`);
+    }
+}
+let invOne = new Invoice("mario", "jumping", 100);
+let invTwo = new Invoice("luigi", "jumping", 80);
+let invoices = [];
+invoices.push(invOne, invTwo);
+console.log(invoices);
+invoices.forEach((inv) => {
+    inv.format();
+});
 const form = document.querySelector(".new-tx-form");
 const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
@@ -7,5 +25,5 @@ const amount = document.querySelector("#amount");
 form.addEventListener('submit', (e) => {
     // prevents loss of data after button clicks
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.value);
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,32 @@
+
+class Invoice {
+    client : string;
+    details : string;
+    amount : number;
+
+    constructor(c : string , d : string , a : number) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format(){
+        console.log(`${this.client} has to pay ${this.amount} for ${this.amount}`);
+    }
+}
+
+let invOne = new Invoice("mario","jumping",100);
+let invTwo = new Invoice("luigi","jumping",80);
+
+let invoices : Invoice[] = [];
+invoices.push(invOne,invTwo);
+
+console.log(invoices);
+
+invoices.forEach((inv)=>{
+    inv.format();
+})
+
 const form = document.querySelector(".new-tx-form") as HTMLFormElement;
 
 const type = document.querySelector("#type") as HTMLSelectElement;
